44f46a6956d12a863d055a16efb982514e411714
Zadiel Salas <sadielsalas35@gmail.com>
2024-12-10 17:01:06 -0400

    first 5

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': '/src/assets', // Alias para acceder a la carpeta 'assets'
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@assets': '/src/assets', // Alias para acceder a la carpeta 'assets'
+		},
+	},
+	base: '/AppSalas/', // Reemplaza con el nombre de tu repositorio
+	build: {
+		outDir: 'dist',
+	},
 });
