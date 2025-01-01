d6ac7407ca0c55bd25bbd63810d3f7600ea338bc
Zadiel Salas <sadielsalas35@gmail.com>
2025-01-01 18:09:19 -0400

    intento dos

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@assets': '/src/assets',
+			'@assets': '/src/assets', // Alias para acceder a la carpeta 'assets'
 		},
 	},
-	base: process.env.NODE_ENV === 'production' ? '/AppSalas/' : '/',
+	base: './', // Reemplaza con el nombre de tu repositorio
 	build: {
 		outDir: 'dist',
 	},
