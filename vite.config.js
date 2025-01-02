818a15d845cb0215080f58300960cbffe18a5570
Zadiel Salas <sadielsalas35@gmail.com>
2025-01-01 23:55:39 -0400

    cambios nuevos

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@assets': '/src/assets',
+			'@assets': '/src/assets', // Alias para acceder a la carpeta 'assets'
 		},
 	},
-	base: process.env.NODE_ENV === 'production' ? '/AppSalas/' : '/',
+	base: '/', // Reemplaza con el nombre de tu repositorio
 	build: {
 		outDir: 'dist',
-	},	
+	},
 });
